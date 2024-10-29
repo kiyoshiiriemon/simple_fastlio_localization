ea291483e5719d2f5edd38a58f3436d488bc33c3
Kiyoshi Irie <kiyoshiiriemon@gmail.com>
2024-10-30 01:30:55 +0900

    ROS2 support

--- src/localization_node.cpp
+++ src/localization_node.cpp
@@ -1,37 +1,51 @@
-#include <ros/ros.h>
-#include <nav_msgs/Odometry.h>
-#include <sensor_msgs/PointCloud2.h>
-#include <geometry_msgs/PoseStamped.h>
-#include <geometry_msgs/TransformStamped.h>
-#include <pcl_conversions/pcl_conversions.h>
-#include <tf2_ros/transform_broadcaster.h>
-
 #include <boost/circular_buffer.hpp>
 #include "simple_lio_loc.h"
 #include "loc_types.h"
 
-class FastLIOHandler
+#include <rclcpp/rclcpp.hpp>
+#include <std_msgs/msg/string.hpp>
+#include <nav_msgs/msg/odometry.hpp>
+#include <sensor_msgs/msg/point_cloud2.hpp>
+#include <geometry_msgs/msg/pose_stamped.hpp>
+#include <geometry_msgs/msg/transform_stamped.hpp>
+#include <pcl_conversions/pcl_conversions.h>
+#include <tf2_ros/transform_broadcaster.h>
+#include <tf2_ros/transform_broadcaster.h>
+#include <Eigen/Geometry>
+#include <pcl/io/pcd_io.h>
+
+class FastLIOHandler : public rclcpp::Node
 {
 public:
-    FastLIOHandler() : nh_("~"), odom_buffer_(10), cloud_buffer_(10)
+    FastLIOHandler() : Node("fast_lio_handler"), odom_buffer_(10), cloud_buffer_(10)
     {
-        odom_sub_  = nh_.subscribe("/Odometry", 10, &FastLIOHandler::odomCallback, this);
-        cloud_sub_ = nh_.subscribe("/cloud_registered_body", 10, &FastLIOHandler::cloudCallback, this);
-        pose_pub_  = nh_.advertise<geometry_msgs::PoseStamped>("/estimated_pose", 10);
-        map_pub_   = nh_.advertise<sensor_msgs::PointCloud2>("/map_cloud", 1, true);
+        this->declare_parameter("map_file", rclcpp::PARAMETER_STRING);
+        this->declare_parameter("initial_pose", rclcpp::PARAMETER_STRING);
+        odom_sub_ = this->create_subscription<nav_msgs::msg::Odometry>(
+            "/Odometry", 10, std::bind(&FastLIOHandler::odomCallback, this, std::placeholders::_1));
+        cloud_sub_ = this->create_subscription<sensor_msgs::msg::PointCloud2>(
+            "/cloud_registered_body", 10, std::bind(&FastLIOHandler::cloudCallback, this, std::placeholders::_1));
+        pose_pub_ = this->create_publisher<geometry_msgs::msg::PoseStamped>("/estimated_pose", 10);
+        map_pub_ = this->create_publisher<sensor_msgs::msg::PointCloud2>("/map_cloud",
+            rclcpp::QoS(rclcpp::KeepLast(1)).transient_local()
+        );
+
         std::string map_file_path;
-        if (!nh_.getParam("map_file", map_file_path))
+        if (!this->get_parameter("map_file", map_file_path))
         {
-            ROS_ERROR("parameter map_file not specified");
-            abort();
+            RCLCPP_ERROR(this->get_logger(), "parameter map_file not specified");
+            rclcpp::shutdown();
+            return;
         }
-        if (loc_.loadMap(map_file_path)) {
+
+        if (loc_.loadMap(map_file_path))
+        {
             publish_map(map_file_path);
         }
 
         Eigen::Isometry3d initial_pose = Eigen::Isometry3d::Identity();
         std::string initial_pose_str;
-        if (nh_.getParam("initial_pose", initial_pose_str))
+        if (this->get_parameter("initial_pose", initial_pose_str))
         {
             initial_pose = parse_posestr(initial_pose_str);
         }
@@ -43,13 +57,15 @@ public:
         pcl::PointCloud<pcl::PointXYZI>::Ptr map_cloud(new pcl::PointCloud<pcl::PointXYZI>());
         if (pcl::io::loadPCDFile<pcl::PointXYZI>(map_file_path, *map_cloud) == -1)
         {
+            std::cerr << "Failed to load map file: " << map_file_path << std::endl;
             return;
         }
 
-        sensor_msgs::PointCloud2 map_msg;
+        sensor_msgs::msg::PointCloud2 map_msg;
         pcl::toROSMsg(*map_cloud, map_msg);
         map_msg.header.frame_id = "map";
-        map_pub_.publish(map_msg);
+        map_pub_->publish(map_msg);
+        std::cerr << "Published map cloud with " << map_cloud->size() << " points" << std::endl;
     }
 
     Eigen::Isometry3d parse_posestr(const std::string &pose_str)
@@ -62,7 +78,7 @@ public:
         Eigen::Isometry3d pose = Eigen::Isometry3d::Identity();
         pose.translation() << x, y, z;
 
-        ROS_INFO("parse pose str trans=(%f %f %f) quat=(%f %f %f %f)", x, y, z, qx, qy, qz, qw);
+        RCLCPP_INFO(this->get_logger(), "parse pose str trans=(%f %f %f) quat=(%f %f %f %f)", x, y, z, qx, qy, qz, qw);
 
         Eigen::Quaterniond q(qw, qx, qy, qz);
         if (q.norm() == 0)
@@ -79,44 +95,44 @@ public:
         return pose;
     }
 
-    void odomCallback(const nav_msgs::Odometry::ConstPtr& msg)
+    void odomCallback(const nav_msgs::msg::Odometry::SharedPtr msg)
     {
-        ROS_INFO("Received Odometry data");
-        const auto& position = msg->pose.pose.position;
-        const auto& orientation = msg->pose.pose.orientation;
-        ROS_INFO("LIO Position: x=%f, y=%f, z=%f", position.x, position.y, position.z);
-        ROS_INFO("LIO Orientation: x=%f, y=%f, z=%f, w=%f", orientation.x, orientation.y, orientation.z, orientation.w);
+        RCLCPP_INFO(this->get_logger(), "Received Odometry data");
+        const auto &position = msg->pose.pose.position;
+        const auto &orientation = msg->pose.pose.orientation;
+        RCLCPP_INFO(this->get_logger(), "LIO Position: x=%f, y=%f, z=%f", position.x, position.y, position.z);
+        RCLCPP_INFO(this->get_logger(), "LIO Orientation: x=%f, y=%f, z=%f, w=%f", orientation.x, orientation.y, orientation.z, orientation.w);
         odom_buffer_.push_back(*msg);
         update();
     }
 
-    void cloudCallback(const sensor_msgs::PointCloud2::ConstPtr& msg)
+    void cloudCallback(const sensor_msgs::msg::PointCloud2::SharedPtr msg)
     {
         simple_lio_localization::PointCloudPCL::Ptr cloud(new simple_lio_localization::PointCloudPCL);
         pcl::fromROSMsg(*msg, *cloud);
 
-        ROS_INFO("Received PointCloud2 data with %ld points", cloud->size());
+        RCLCPP_INFO(this->get_logger(), "Received PointCloud2 data with %ld points", cloud->size());
         cloud_buffer_.push_back(cloud);
         update();
     }
 
     void update()
     {
-        int odom_size = odom_buffer_.size();
-        int cloud_size = cloud_buffer_.size();
-        ROS_INFO("Odom buffer size: %d, Cloud buffer size: %d", odom_size, cloud_size);
-        if (odom_buffer_.empty() || cloud_buffer_.empty()) {
-            ROS_INFO("No data to update");
+        if (odom_buffer_.empty() || cloud_buffer_.empty())
+        {
+            RCLCPP_INFO(this->get_logger(), "No data to update");
             return;
         }
-        const auto& odom = odom_buffer_.front();
-        const auto& cloud = cloud_buffer_.front();
+
+        const auto &odom = odom_buffer_.front();
+        const auto &cloud = cloud_buffer_.front();
         simple_lio_localization::Pose3d lio_pose = simple_lio_localization::Pose3d::Identity();
         lio_pose.translation() << odom.pose.pose.position.x, odom.pose.pose.position.y, odom.pose.pose.position.z;
         lio_pose.rotate(Eigen::Quaterniond(odom.pose.pose.orientation.w, odom.pose.pose.orientation.x, odom.pose.pose.orientation.y, odom.pose.pose.orientation.z));
         loc_.update(*cloud, lio_pose);
         Eigen::Isometry3d pose = loc_.getPose();
-        ROS_INFO("Pose: x=%f, y=%f, z=%f", pose.translation().x(), pose.translation().y(), pose.translation().z());
+
+        RCLCPP_INFO(this->get_logger(), "Pose: x=%f, y=%f, z=%f", pose.translation().x(), pose.translation().y(), pose.translation().z());
         odom_buffer_.pop_front();
         cloud_buffer_.pop_front();
 
@@ -124,8 +140,9 @@ public:
         publish_transform(odom.header.stamp, loc_.getLIOToMap());
     }
 
-    void publish_estimated_pose(const ros::Time &stamp, const simple_lio_localization::Pose3d &pose) {
-        geometry_msgs::PoseStamped pose_msg;
+    void publish_estimated_pose(const rclcpp::Time &stamp, const simple_lio_localization::Pose3d &pose)
+    {
+        geometry_msgs::msg::PoseStamped pose_msg;
         pose_msg.header.stamp = stamp;
         pose_msg.header.frame_id = "map";
         pose_msg.pose.position.x = pose.translation().x();
@@ -136,50 +153,42 @@ public:
         pose_msg.pose.orientation.x = q.x();
         pose_msg.pose.orientation.y = q.y();
         pose_msg.pose.orientation.z = q.z();
-        pose_pub_.publish(pose_msg);
+        pose_pub_->publish(pose_msg);
     }
 
-    void publish_transform(const ros::Time &stamp, const simple_lio_localization::Pose3d &lio_to_map) {
-        geometry_msgs::TransformStamped transformStamped;
-
+    void publish_transform(const rclcpp::Time &stamp, const simple_lio_localization::Pose3d &lio_to_map)
+    {
+        geometry_msgs::msg::TransformStamped transformStamped;
         transformStamped.header.stamp = stamp;
         transformStamped.header.frame_id = "map";
         transformStamped.child_frame_id = "camera_init";
-
         transformStamped.transform.translation.x = lio_to_map.translation().x();
         transformStamped.transform.translation.y = lio_to_map.translation().y();
         transformStamped.transform.translation.z = lio_to_map.translation().z();
-        geometry_msgs::Quaternion orientation;
         Eigen::Quaterniond q(lio_to_map.rotation());
-        orientation.x = q.x();
-        orientation.y = q.y();
-        orientation.z = q.z();
-        orientation.w = q.w();
-        transformStamped.transform.rotation = orientation;
-
-        tf_broadcaster_.sendTransform(transformStamped);
+        transformStamped.transform.rotation.x = q.x();
+        transformStamped.transform.rotation.y = q.y();
+        transformStamped.transform.rotation.z = q.z();
+        transformStamped.transform.rotation.w = q.w();
+        tf_broadcaster_->sendTransform(transformStamped);
     }
 
 private:
     simple_lio_localization::SimpleLIOLoc loc_;
-    ros::NodeHandle nh_;
-    ros::Subscriber odom_sub_;
-    ros::Subscriber cloud_sub_;
-    ros::Publisher pose_pub_;
-    ros::Publisher map_pub_;
-    tf2_ros::TransformBroadcaster tf_broadcaster_;
-    boost::circular_buffer<nav_msgs::Odometry> odom_buffer_;
+    rclcpp::Subscription<nav_msgs::msg::Odometry>::SharedPtr odom_sub_;
+    rclcpp::Subscription<sensor_msgs::msg::PointCloud2>::SharedPtr cloud_sub_;
+    rclcpp::Publisher<geometry_msgs::msg::PoseStamped>::SharedPtr pose_pub_;
+    rclcpp::Publisher<sensor_msgs::msg::PointCloud2>::SharedPtr map_pub_;
+    std::unique_ptr<tf2_ros::TransformBroadcaster> tf_broadcaster_ = std::make_unique<tf2_ros::TransformBroadcaster>(this);
+    boost::circular_buffer<nav_msgs::msg::Odometry> odom_buffer_;
     boost::circular_buffer<simple_lio_localization::PointCloudPCL::Ptr> cloud_buffer_;
 };
 
-int main(int argc, char** argv)
+int main(int argc, char **argv)
 {
-    ros::init(argc, argv, "fast_lio_handler_node");
-
-    FastLIOHandler handler;
-
-    ros::spin();
-
+    rclcpp::init(argc, argv);
+    auto node = std::make_shared<FastLIOHandler>();
+    rclcpp::spin(node);
+    rclcpp::shutdown();
     return 0;
 }
-
